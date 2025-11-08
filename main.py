8e9adbb5c4aa65b63ae52d8f954ef97dff7a10be
Projects-codelive <sg790540@gmail.com>
2025-11-08 11:44:21 +0530

    Data Validation Module Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -11,6 +12,21 @@ if __name__ == '__main__':
         data_ingestion = DataIngestion(datainegestionconfig)
         logging.info("Data Ingestion Artifact Created")
         datainegestionartifact =  data_ingestion.initiate_data_ingestion()
+        logging.info("Data Ingestion Completed")
         print(datainegestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(datainegestionartifact, data_validation_config)
+        logging.info("Initiate Data Validation Artifact")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
+
     except NetworkSecurityException as e:
         raise NetworkSecurityException(e,sys)
+
+
+# Data Valiadtion includes
+# 1. Schema reamains the same i.e No. of columns(features) remain the same
+# 2. Data Drift change in data
+# 3. Validate No. of column
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -1,5 +1,16 @@
 from dataclasses import dataclass
+# Output of Data Ingestion
 @dataclass
 class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
+
+# Output of data Validation
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
