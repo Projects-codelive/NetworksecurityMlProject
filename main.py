23dfd3493ea1063186c7082ed15642c0b9bd7be7
Projects-codelive <sg790540@gmail.com>
2025-12-06 23:14:44 +0530

    Model Training Completed

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, \
+    ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -31,5 +33,10 @@ if __name__ == "__main__":
         logging.info("Data Transformation Completed")
         print(data_transformation_artifact)
 
+        logging.info("Initiate Model Training Artifact")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer =ModelTrainer(model_trainer_config, data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Training Completed")
     except NetworkSecurityException as e:
         raise NetworkSecurityException(e, sys)
